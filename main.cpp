627e8178b380975e1b7c318b6d2d7b6f090b9b02
Josep Marcello <jspmarcello@live.com>
2021-01-21 01:42:15 +0700

    feat: move function declarations to a header file

--- main.cpp
+++ main.cpp
@@ -3,80 +3,17 @@
  * Tanggal  : 20 Januari 2021
  */
 
-#include <iostream>
 #include <fstream>
-#include <vector>
 #include <unordered_map>
 #include <sysexits.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "funcs.hpp"
 
 #define debug1() puts("males belajar tapi...")
 #define debug2() puts("pengen kaya")
 #define debug3() puts("haha lucu lo badut")
 #define MAX_UNIQUE_LETTERS 10
-
-// *** DEKLARASI FUNGSI-FUNGSI ***
-
-/**
- * Fungsi untuk menghapuskan whitespaces ('', '\t', '\n') dari awal C string.
- *
- * @param *strToStrip pointer ke C string yang ingin di-strip
- * @returns std::string yang sudah dihapuskan whitespace-nya
- */
-std::string stripAtBeginning(char* strToStrip);
-
-/**
- * Fungsi untuk menuliskan isi vector.
- * Format: [el1, el2, el3, el4,]
- *
- * @tparam T elemen yang ditampung vector.
- * @param vec vector yang ingin dituliskan isinya
- */
-template <class T>
-void printVec(std::vector<T> vec);
-/**
- * Fungsi untuk menuliskan isi vector multidimensi
- * Format:
- * [
- *		[el11, el12, el13, el14,],
- *		[el21, el22, el23, el24,],
- *		[el31, el32, el33, el34,],
- *		[el41, el42, el43, el44,],
- * ]
- *
- * @tparam T elemen yang ditampung vector.
- * @param vecOvec vector dari vector yang ingin dituliskan isinya
- *
- * @overload
- */
-template <class T>
-void printVec(std::vector<std::vector<T>> vecOvec);
-
-/**
- * Fungsi untuk membaca file (sesuai format pada spek) lalu memisahkannya
- * berdasarkan soal
- *
- * @param *fileName string yang berisi nama file soal
- * @param *output vector dari vector yang menampung soal-soal (tiap elemen
- * adalah soal)
- */
-void parseFile(char* fileName, std::vector<std::vector<std::string>>* output);
-
-/**
- * Fungsi untuk mendekripsi Cryptarithmetic
- *
- * @param soal soal yang mau didekripsi
- */
-void decryptCryparithm(std::vector<std::string> soal);
-
-/**
- * Fungsi untuk mendapatkan huruf-huruf unik dari soal
- *
- * @param soal soal yang ingin dicari huruf-huruf uniknya
- */
-std::vector<char> uniqueLetters(std::vector<std::string> soal);
-// *** END ***
 
 int main(int argc, char *argv[])
 {
